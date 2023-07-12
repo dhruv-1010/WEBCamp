36f54cb34be3dba6cf36ce4734c0ed59ab462b0a
Dhruv <dhruv.singh_cs21@gla.ac.in>
2023-07-12 19:34:48 +0530

    July

---
+++ jovac/Day 13 AJAX_api/script.js
@@ -0,0 +1,12 @@
+let input = document.querySelector('input');
+let btn = document.querySelector('button');
+let list = document.getElementById('list');
+btn.addEventListener('click',()=>{
+
+})
+function fetchData(searchText){
+    axios.get()
+    .then(function(response){
+        
+    })
+}
